5c1159bd2b54d2c23c2b460cdaedac2ba02c6e13
SleepyDude <ko_on@mail.ru>
2023-01-07 21:22:04 +0800

    chore 4

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,15 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
+from app.utils import get_password_hash
 
 
 def test_root():
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_hash():
+    password = '12345'
+    hashed_pass = get_password_hash(password)
+    assert type(hashed_pass) == str
+
