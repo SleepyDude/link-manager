59224e005778aec2b82f9d6a6f71a118dbc733f7
SleepyDude <ko_on@mail.ru>
2023-01-08 14:20:48 +0800

    delete passlib because not works on AWS, use bcrypt directly

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,9 @@
-from passlib.context import CryptContext
+import bcrypt
 
-# cryptography
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt.checkpw(plain_password.encode('utf-8'), 
+        hashed_password.encode('utf-8'))
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
+def get_password_hash(password: str):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())\
+        .decode('utf-8')
