ede715f462334b3120bfb2c243965662f1eaa441
SleepyDude <ko_on@mail.ru>
2023-01-13 21:59:56 +0800

    link endpoints and start tag system

--- app/api/links.py
+++ app/api/links.py
@@ -6,7 +6,11 @@ from datetime import datetime
 from ..models.link_mod import PutLinkRequest, Link, LinkListParams, LinkInp
 from ..models.user_mod import User
 from .users import get_current_user
-from ..db import db_get_links_by_user, db_put_link
+from ..db import (
+    db_get_links_by_user, db_put_link,
+    db_get_link_by_url, db_get_link_by_id,
+    _db_add_tag_to_link, _db_delete_tag_from_link
+)
 
 router = APIRouter()
 
@@ -18,10 +22,43 @@ async def get_links(query: LinkListParams = Depends(),
         cur_user.username, query.limit, query.offset)
     return linklist
 
+@router.get('/get_link_by_url')
+async def get_link_by_url(
+    url: str,
+    cur_user: User = Depends(get_current_user)
+):
+    link = db_get_link_by_url(username=cur_user.username, url=url)
+    return link
+
+@router.get('/get_link_by_timestamp')
+async def get_link_by_timestamp(
+    timestamp: str,
+    cur_user: User = Depends(get_current_user)
+):
+    link = db_get_link_by_id(username=cur_user.username, id=timestamp)
+    return link
+
 @router.post('/add_link')
 async def add_link(link_inp: LinkInp, cur_user: User = Depends(get_current_user)):
     l = db_put_link(username=cur_user.username, link_inp=link_inp)
     return {'Message': l.dict()}
+
+@router.post('/add_tag')
+async def add_tag(link_timestamp: str, tagname: str,
+    cur_user: User = Depends(get_current_user)
+):
+    err_msg = _db_add_tag_to_link(username=cur_user.username,
+        link_timestamp=link_timestamp, tagname=tagname)
+    if err_msg is not None:
+        return err_msg
+    return {'Message': 'Added tag'}
+
+@router.delete('/remove_tag')
+async def remove_tag(link_timestamp: str, tagname: str,
+    cur_user: User = Depends(get_current_user)
+):
+    _db_delete_tag_from_link(cur_user.username,
+        link_timestamp=link_timestamp, tagname=tagname)
 
 '''
 # TODO made dependaple of access token (login)
